7385345871e3e9303d16fae6ff575dc6ecfd19ee
Lukasz Rydziński <rlukasz1996@gmail.com>
2019-06-21 17:28:34 +0200

    VD || Questions System

---
+++ WhiteHat_VD/Assets/Scripts/PanelController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelController : MonoBehaviour
+{
+    public QuestionController questionController;
+    public GameObject player;
+
+    private QuestionBase questionBase;
+
+    public Text question;
+    public Button ans_A;
+    public Button ans_B;
+    public Button ans_C;
+    public Button ans_D;
+
+    public void ChangeVisibility()
+    {
+        this.gameObject.SetActive(!(this.gameObject.activeSelf));
+        Debug.Log(this.gameObject.name + " is " + this.gameObject.activeSelf);
+    }
+
+    public void SetVisibility(bool isVisible)
+    {
+        this.gameObject.SetActive(isVisible);
+    }
+
+    public void SetQuestion()
+    {
+        questionBase = questionController.RandQuestion();
+
+        question.text = questionBase.question;
+        ans_A.GetComponentInChildren<Text>().text = questionBase.ansA;
+        ans_B.GetComponentInChildren<Text>().text = questionBase.ansB;
+        ans_C.GetComponentInChildren<Text>().text = questionBase.ansC;
+        ans_D.GetComponentInChildren<Text>().text = questionBase.ansD;
+    }
+
+    public void CheckAnswer(int numberOfButton)
+    {
+        if (numberOfButton == questionBase.correctAns)
+        {
+            player.SetActive(true);
+            ChangeVisibility();
+        }
+        else
+        {
+            SetQuestion();
+        }
+            
+    }
+
+}
---
+++ WhiteHat_VD/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public PanelController panelController;
+
+    public GameObject trapGameObject;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision with " + collision.gameObject.tag);
+
+        QuestionCollisionBehaviour(collision);
+        TrapCollisionBehaviour(collision);
+
+    }
+
+    private void QuestionCollisionBehaviour(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Question")
+        {
+            if (!panelController.gameObject.activeSelf)
+            {
+                panelController.SetVisibility(true);
+                panelController.SetQuestion();
+                Destroy(collision.gameObject);
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void TrapCollisionBehaviour(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Trap")
+        {
+            trapGameObject.gameObject.SetActive(true);
+        }
+    }
+
+}
