4890984f1ce7ce8bb794488b9d8450854c15b128
Lukasz Rydziński <rlukasz1996@gmail.com>
2019-06-21 12:01:50 +0200

    VD || MainMenu

---
+++ WhiteHat_VD/Assets/MainMenu/Scripts/AppController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppController : MonoBehaviour
+{
+
+    public void ExitApplication()
+    {
+        Debug.Log("Application Exit");
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
