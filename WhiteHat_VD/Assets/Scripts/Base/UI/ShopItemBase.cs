28cfdd44f16594807e9d3bc385fe4965c848653a
Łukasz Rydziński <rlukasz1996@gmail.com>
2019-07-29 15:23:12 +0200

    VD || ShopItem Update

---
+++ WhiteHat_VD/Assets/Scripts/Base/UI/ShopItemBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopItemBase : MonoBehaviour
+{
+    public int itemId;
+    public string itemName;
+    public int itemPrice;
+    
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
