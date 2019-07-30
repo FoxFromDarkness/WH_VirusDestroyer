47b70952b28a37220714227ef613bfeb8e39a329
Łukasz Rydziński <rlukasz1996@gmail.com>
2019-07-30 13:52:23 +0200

    VD || LoadingScreen

--- WhiteHat_VD/Assets/Scripts/Controllers/SceneController.cs
+++ WhiteHat_VD/Assets/Scripts/Controllers/SceneController.cs
@@ -5,10 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    public LoadingScreenController loadingScreen;
+
     public void LoadScene(string sceneName)
     {
         //SceneManager.LoadScene("_RootScene");
-        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        StartCoroutine(LoadSceneAsynchronously(sceneName));
+        
+    }
+
+    private IEnumerator LoadSceneAsynchronously (string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+        loadingScreen.gameObject.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            yield return new WaitForSeconds(0.2f);
+            loadingScreen.SetSliderValue(operation.progress);
+            yield return new WaitForSeconds(0.2f);//null;
+        }
+
+        loadingScreen.gameObject.SetActive(false);
     }
 
 }
---
+++ WhiteHat_VD/Assets/Scripts/Controllers/UI/LoadingScreenController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingScreenController : MonoBehaviour
+{
+    private Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.SetActive(false);
+
+        slider = GetComponentInChildren<Slider>();
+    }
+
+     public void SetSliderValue(float value)
+    {
+        slider.value = value;
+    }
+}
