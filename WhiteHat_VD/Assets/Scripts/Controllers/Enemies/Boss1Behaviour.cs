d5fe08ee452e53e2cf92ab6652f95b770af92d28
Łukasz Rydziński <rlukasz1996@gmail.com>
2019-09-05 12:49:38 +0200

    VD || Boss1 Update

---
+++ WhiteHat_VD/Assets/Scripts/Controllers/Enemies/Boss1Behaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss1Behaviour : MonoBehaviour
+{ 
+    public float speedOfRotation;
+    public float speedOfMoving;
+    private PlayerController player;
+
+    //Moving
+    private int movingDirection = 1;
+
+    //PlayerFollowing
+    private Vector2 direction;
+    private float angle;
+    private Quaternion rotation;
+
+    private void Start() {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Moving();
+        PlayerFollowing();
+    }
+
+    private void PlayerFollowing() {
+        direction = -player.transform.position + this.transform.position;
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speedOfRotation * Time.deltaTime);
+    }
+
+    private void Moving() {
+        Vector3 direction = new Vector3(0, movingDirection, 0);
+        transform.Translate(direction * speedOfMoving * Time.deltaTime);
+    }
+}
---
+++ WhiteHat_VD/Assets/Scripts/Levels/BossLevel1.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossLevel1 : MonoBehaviour
+{
+    private void Start() {
+        Camera.main.orthographicSize *= 3.0f;
+    }
+}
