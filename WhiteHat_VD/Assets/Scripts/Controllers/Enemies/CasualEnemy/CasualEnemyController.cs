44d3b5b2df1b9302d1a4e86b2463cd27d440105e
Łukasz Rydziński <rlukasz1996@gmail.com>
2020-01-28 00:05:15 +0100

    || Enemy update -> patrol & idle

---
+++ WhiteHat_VD/Assets/Scripts/Controllers/Enemies/CasualEnemy/CasualEnemyController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CasualEnemyController : MonoBehaviour
+{
+    public enum Type
+    {
+        PATROL,
+        GUARD,
+        KAMIKADZE,
+        NONE
+    }
+
+    public enum Movement
+    {
+        IDLE,
+        ATTACK,
+        DIE,
+        NONE
+    }
+
+    [SerializeField] private Transform body;
+    private SpriteRenderer bodyImage;
+    [SerializeField] private Type enemyType;
+    [SerializeField] private Movement enemyMovement = Movement.IDLE;
+    [SerializeField] float enemySpeed = 2;
+    [SerializeField] bool movingRight = true;
+    [SerializeField] bool isActive = true;
+
+    private void Start()
+    {
+        bodyImage = body.GetComponent<SpriteRenderer>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isActive) return;
+
+        switch (enemyMovement)
+        {
+            case Movement.IDLE:
+                Idle();
+                break;
+            case Movement.ATTACK:
+                break;
+            case Movement.DIE:
+                break;
+            case Movement.NONE:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Idle()
+    {
+        switch (enemyType)
+        {
+            case Type.PATROL:
+                body.Translate(enemySpeed * Time.deltaTime * (movingRight == true ? 1 : -1), 0, 0);
+                break;
+            case Type.GUARD:
+                break;
+            case Type.KAMIKADZE:
+                break;
+            case Type.NONE:
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void BorderCollision()
+    {
+        switch (enemyType)
+        {
+            case Type.PATROL:
+                movingRight = !movingRight;
+                bodyImage.flipX = !bodyImage.flipX;
+                break;
+            case Type.GUARD:
+                break;
+            case Type.KAMIKADZE:
+                break;
+            case Type.NONE:
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ WhiteHat_VD/Assets/Scripts/Controllers/Enemies/CasualEnemy/EnemyTriggerBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTriggerBehaviour : MonoBehaviour
+{
+    private CasualEnemyController enemyController;
+
+    private void Start()
+    {
+        enemyController = GetComponentInParent<CasualEnemyController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<EnemyBorder>())
+            enemyController.BorderCollision();
+    }
+}
